b1b225dfb58a123ffe7aaac87224aeac4a1c222a
Woong <ywcsuwon@naver.com>
2023-05-28 15:30:59 +0900

    (baekjoon)10813

--- src/백준/B_10810.java
+++ src/백준/B_10810.java
@@ -6,26 +6,24 @@ public class B_10810 {
 
 	public static void main(String[] args) {
 		Scanner sc =  new Scanner(System.in);
-		int i = 0;
-		int j = 0;
-		int k = 0;
+		int temp = 0;
 		int N = sc.nextInt();
 		int M = sc.nextInt();
 		int[]buket = new int[N];
-		
-		for(int e=0 ; e<M ; e++) {
-			i = sc.nextInt();
-			j = sc.nextInt();
-			k = sc.nextInt();
-			for(int f=i ; f<j+1 ; f++) {
-				buket[f-1] = k;
-			}
+		for(int i=0 ; i<buket.length ; i++) {
+			buket[i] = i+1;
 		}
 		
-		for(int e=0; e<buket.length ; e++) {
-				System.out.print(buket[e] + " ");
-			}
-		
+		for(int i=0 ; i<M ; i++) {
+			int j = sc.nextInt();
+			int k = sc.nextInt();
+			temp = buket[j-1];
+			buket[j-1] = buket[k-1];
+			buket[k-1] = temp;
+		}
+		for(int i=0 ; i<buket.length ; i++) {
+			System.out.print(buket[i] + " ");
+		}
 	}
 
 }
