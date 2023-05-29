0a46420b578c29ef319839a8f07f409274b09051
Woong <ywcsuwon@naver.com>
2023-05-30 01:38:34 +0900

    String

---
+++ src/백준/B_27866.java
@@ -0,0 +1,13 @@
+package น้มุ;
+
+import java.util.Scanner;
+
+public class B_27866 {
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		int i = sc.nextInt();
+		System.out.println(s.charAt(i-1));
+}
+}
