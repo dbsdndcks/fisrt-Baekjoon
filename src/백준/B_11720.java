9418693654a883f4363ba20e1492aec32c424d02
Woong <ywcsuwon@naver.com>
2023-06-02 00:02:24 +0900

    today

---
+++ src/백준/B_11720.java
@@ -0,0 +1,22 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class B_11720 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br.readLine();	// N 은 쓸모가 없으므로 입력만 받는다.
+		
+		int sum = 0;
+		
+		for(byte value : br.readLine().getBytes()) {
+			sum += (value - '0');	// 또는 (a-48)
+		}
+		
+		System.out.print(sum);
+	}
+}
