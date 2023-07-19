961480e0f38dd507fb4ff0eb3f28101f26ff77b6
Woong <ywcsuwon@naver.com>
2023-07-19 11:51:20 +0900

    new

---
+++ src/Baekjoon/B_2903.java
@@ -0,0 +1,14 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class B_2903 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		System.out.println((int)(Math.pow(Math.pow(2, N)+1, 2)));
+	}
+
+}
