dda6e3be870d01adc6aa6ed0dac7a029fc8b879c
Woong <ywcsuwon@naver.com>
2023-11-19 20:36:37 +0900

    'ddd'

---
+++ src/Baekjoon/B_24262.java
@@ -0,0 +1,17 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_24262 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		System.out.println(1);
+		System.out.println(0);
+	}
+
+}
---
+++ src/Baekjoon/B_24264.java
@@ -0,0 +1,16 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_24264 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Long N = Long.parseLong(br.readLine());
+		
+		
+		System.out.println(N*N);
+		System.out.println(2);
+	}
+}
