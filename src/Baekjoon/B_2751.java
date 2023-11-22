4500860e15a758b66277a37d8943c6548d3bc820
Woong <ywcsuwon@naver.com>
2023-11-22 17:04:38 +0900

    2023-11-22

--- src/Baekjoon/B_2751.java
+++ src/Baekjoon/B_2751.java
@@ -1,26 +1,32 @@
 package Baekjoon;
 
+import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-
+import java.util.ArrayList;
+import java.util.Collections;
+ 
+ 
 public class B_2751 {
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
+    
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
 		int N = Integer.parseInt(br.readLine());
-		int arr[] = new int[N];
 		
-		for(int i =0; i < N; i++) {
-			arr[i] = Math.abs(Integer.parseInt(br.readLine()));
-		}
-		
-		Arrays.sort(arr);
+		// list 계열 중 하나를 쓰면 된다.
+		ArrayList<Integer> list = new ArrayList<>();
 		
 		for(int i = 0; i < N; i++) {
-			System.out.println(arr[i]);
+			list.add(Integer.parseInt(br.readLine()));
 		}
+		
+		Collections.sort(list);
+		
+		for(int value : list) {
+			sb.append(value).append('\n');
+		}
+		System.out.println(sb);
 	}
-
 }
